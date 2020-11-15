2ba0099514cb5915f311758d36a6cc86884db7c3
ValeriiMatviienko <unanimous276@gmail.com>
2020-11-15 15:50:39 +0200

    решает задачи

---
+++ js/task-2.js
@@ -0,0 +1,23 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const listForAddingItems = document.querySelector(`#ingredients`);
+
+
+
+const addListItems = (array) => {
+    const listArray = array.map(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = item;
+        return listItem;
+    })
+    listForAddingItems.append(...listArray);
+}
+
+addListItems(ingredients);
